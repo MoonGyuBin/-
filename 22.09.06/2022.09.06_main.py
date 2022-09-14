ac8d65795075cf67a3386fe4231783d25c045e1d
MoonGyuBin <2019captain@gmail.com>
2022-09-14 20:49:04 +0900

    내일배움캠프 이창호 튜터님 파이썬 숙제 22.09.06

---
+++ 22.09.06/2022.09.06_main.py
@@ -0,0 +1,7 @@
+import a
+
+num1 = int(input())
+num2 = int(input())
+operator = input()
+
+a.cal(num1, num2, operator)
---
+++ 22.09.06/22.09.06_1.py
@@ -0,0 +1,13 @@
+def cal(num1, num2, operator):
+    if operator == "+":
+        result1 = num1 + num2
+        print(result1)
+    elif operator == "-":
+        result2 = num1 - num2
+        print(result2)
+    elif operator == "*":
+        result3 = num1 * num2
+        print(result3)
+    elif operator == "/":
+        result4 = num1 / num2
+        print(result4)
